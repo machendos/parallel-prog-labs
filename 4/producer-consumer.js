6ae2a5618f9f847e5123a3064f9ef33fca3e6797
Nikita Machekhin <nicitamac@gmail.com>
2020-09-22 21:06:58 +0300

    Separate logger

--- 4/producer-consumer.js
+++ 4/producer-consumer.js
@@ -7,30 +7,39 @@
 // waiting for at least one product to be bought by some consumer
 // There are a number of things to do in total (TOTAL_PRODUCTS_COUNT)
 
-// Task: create model using ReentrantLock (5 variant). In JavaScript,
-// analogue of ReentrantLock is Mutex
+// Task: 5 variant, ReentrantLock
 
-const Mutex = require(__dirname + '/mutex');
+const ReentrantLock = require(__dirname + '/reentrant-lock');
 const thread = require('worker_threads');
 const fs = require('fs');
+
+const { Worker } = thread;
 
 const TOTAL_PRODUCTS_COUNT = 10;
 const SHOP_CAPACITY = 3;
 const PRODUCERS_COUNT = 2;
 const CONSUMERS_COUNT = 2;
 
-const { Worker } = thread;
+const log = (n, before, after, ind, act) => {
+  fs.writeFileSync(
+    __dirname + '/results-1.txt',
+    `${n} ${act} product. Before: ${before}. After: ${after} Индекс: ${ind}\n`,
+    { flag: 'a' }
+  );
+};
+
+log.init = () => fs.writeFileSync(__dirname + '/results-1.txt', '');
 
 if (thread.isMainThread) {
-  fs.writeFileSync(__dirname + '/results.txt', '');
-  const mutexBuffer = new SharedArrayBuffer(4);
-  new Mutex(mutexBuffer, 0, true);
+  log.init();
+  const lockerBuffer = new SharedArrayBuffer(4);
+  new ReentrantLock(lockerBuffer, 0, true);
 
   const shopBuffer = new SharedArrayBuffer(16); // busy, produced, consumed, ind
   for (let producerNumb = 0; producerNumb < PRODUCERS_COUNT; producerNumb++) {
     new Worker(__filename, {
       workerData: {
-        mutexBuffer,
+        lockerBuffer,
         shopBuffer,
         producer: true,
         name: `producer${producerNumb}`,
@@ -40,7 +49,7 @@ if (thread.isMainThread) {
   for (let consumerNumb = 0; consumerNumb < CONSUMERS_COUNT; consumerNumb++) {
     new Worker(__filename, {
       workerData: {
-        mutexBuffer,
+        lockerBuffer,
         shopBuffer,
         producer: false,
         name: `consumer${consumerNumb}`,
@@ -48,38 +57,26 @@ if (thread.isMainThread) {
     });
   }
 } else {
-  const { mutexBuffer, shopBuffer, producer, name } = thread.workerData;
-  const mutex = new Mutex(mutexBuffer);
+  const { lockerBuffer, shopBuffer, producer, name } = thread.workerData;
+  const locker = new ReentrantLock(lockerBuffer);
   const shop = new Int32Array(shopBuffer);
   if (producer) {
     while (shop[1] < TOTAL_PRODUCTS_COUNT) {
-      mutex.enter();
+      locker.enter();
       if (shop[0] < SHOP_CAPACITY && shop[1] < TOTAL_PRODUCTS_COUNT) {
-        fs.writeFileSync(
-          __dirname + '/results.txt',
-          `${name} produced product. Before: ${
-            shop[0]
-          }. After: ${++shop[0]} Индекс: ${Atomics.add(shop, 3, 1) + 1}\n`,
-          { flag: 'a' }
-        );
+        log(name, shop[0]++, shop[0], ++shop[3], 'produced');
         shop[1]++;
       }
-      mutex.leave();
+      locker.leave();
     }
   } else {
     while (shop[2] < TOTAL_PRODUCTS_COUNT) {
-      mutex.enter();
+      locker.enter();
       if (shop[0] > 0 && shop[2] < TOTAL_PRODUCTS_COUNT) {
-        fs.writeFileSync(
-          __dirname + '/results.txt',
-          `${name} consumed product. Before: ${
-            shop[0]
-          }. After: ${--shop[0]} Индекс: ${Atomics.add(shop, 3, 1) + 1}\n`,
-          { flag: 'a' }
-        );
+        log(name, shop[0]--, shop[0], ++shop[3], 'consumed');
         shop[2]++;
       }
-      mutex.leave();
+      locker.leave();
     }
   }
 }
