5ddca7ae37673dba1acad535ec6ee6d0543a0658
Nikita Machekhin <nicitamac@gmail.com>
2020-09-22 21:35:50 +0300

    Add solve of reader-writer pronlem

---
+++ 4/reader-writer.js
@@ -0,0 +1,103 @@
+'use strict';
+
+const threads = require('worker_threads');
+const Semaphore = require(__dirname + '/binary-semaphore.js');
+const writeFileSync = require('fs').writeFileSync;
+
+const { Worker } = threads;
+
+const AVERAGE_PERCENTAGE_OF_READERS = 0.3;
+const THREADS_COUNT = 10;
+const TOTAL_OPERATION_COUNT = 10000;
+
+const log = (data1, data2) =>
+  writeFileSync(__dirname + '/results-2.txt', data1 + data2, { flag: 'a' });
+
+const writerCallback = threadName =>
+  log(`${threadName} --> WRITE START\n`, `${threadName} --> WRITE FINISH\n`);
+
+const readCallback = threadName =>
+  log(`${threadName} --> READ START\n`, `${threadName} --> READ FINISH\n`);
+
+if (threads.isMainThread) {
+  writeFileSync(__dirname + '/results-2.txt', '');
+  const WBRSemaphoreBuffer = new SharedArrayBuffer(4);
+  new Semaphore(WBRSemaphoreBuffer, 0, true);
+
+  const freeSemaphoreBuffer = new SharedArrayBuffer(4);
+  new Semaphore(freeSemaphoreBuffer, 0, true);
+
+  const changeReadersBuffer = new SharedArrayBuffer(4);
+  new Semaphore(changeReadersBuffer, 0, true);
+
+  const operationCounterBuffer = new SharedArrayBuffer(4);
+  const readersCounterBuffer = new SharedArrayBuffer(4);
+  const writersCounterBuffer = new SharedArrayBuffer(4);
+
+  for (let workerNumb = 0; workerNumb < THREADS_COUNT; workerNumb++) {
+    new Worker(__filename, {
+      workerData: {
+        WBRSemaphoreBuffer,
+        freeSemaphoreBuffer,
+        changeReadersBuffer,
+        operationCounterBuffer,
+        readersCounterBuffer,
+        writersCounterBuffer,
+      },
+    });
+  }
+} else {
+  const {
+    WBRSemaphoreBuffer,
+    freeSemaphoreBuffer,
+    changeReadersBuffer,
+    operationCounterBuffer,
+    readersCounterBuffer,
+    writersCounterBuffer,
+  } = threads.workerData;
+
+  const WBRMutex = new Semaphore(WBRSemaphoreBuffer);
+  const freeMutex = new Semaphore(freeSemaphoreBuffer);
+  const changeReaders = new Semaphore(changeReadersBuffer);
+  const operationCounter = new Int32Array(operationCounterBuffer);
+  const readersCounter = new Int32Array(readersCounterBuffer);
+  const writersCounter = new Int32Array(writersCounterBuffer);
+
+  while (Atomics.load(operationCounter, 0) < TOTAL_OPERATION_COUNT) {
+    const read = Math.random() > AVERAGE_PERCENTAGE_OF_READERS;
+    if (read) {
+      while (true) {
+        WBRMutex.enter();
+        if (Atomics.load(writersCounter, 0) > 0) {
+          WBRMutex.leave();
+        } else {
+          changeReaders.enter();
+          Atomics.add(readersCounter, 0, 1);
+          if (!freeMutex.isLocked()) {
+            freeMutex.enter();
+          }
+          changeReaders.leave();
+          WBRMutex.leave();
+          readCallback(threads.threadId);
+          Atomics.add(operationCounter, 0, 1);
+          changeReaders.enter();
+          Atomics.sub(readersCounter, 0, 1);
+          if (Atomics.load(readersCounter, 0) === 0) {
+            freeMutex.leave();
+          }
+          changeReaders.leave();
+          break;
+        }
+      }
+    } else {
+      Atomics.add(writersCounter, 0, 1);
+      WBRMutex.enter();
+      freeMutex.enter();
+      writerCallback(threads.threadId);
+      Atomics.add(operationCounter, 0, 1);
+      Atomics.sub(writersCounter, 0, 1);
+      freeMutex.leave();
+      WBRMutex.leave();
+    }
+  }
+}
