416c9217e7da6ed34633878247ed2e80ffddcffd
Nikita Machekhin <nicitamac@gmail.com>
2020-09-25 16:11:32 +0300

    Create model class

--- 2/index.js
+++ 2/index.js
@@ -1,79 +1,15 @@
 'use strict';
 
-const thread = require('worker_threads');
 const join = require('path').join;
 const CPU = require(join(__dirname, 'CPU.js'));
 const CPUQueue = require(join(__dirname, 'queue.js'));
-
-const { Worker } = thread;
-
-const FIRST_PROCESS_NAME = 1;
-const FIRST_PROCESS_INTERVAL_BOTTON = 100;
-const FIRST_PROCESS_INTERVAL_TOP = 200;
-const FIRST_PROCESS_COUNT = 30;
-
-const SECOND_PROCESS_NAME = 2;
-const SECOND_PROCESS_INTERVAL_BOTTON = 100;
-const SECOND_PROCESS_INTERVAL_TOP = 200;
-const SECOND_PROCESS_COUNT = 30;
-
-const firstProcess = new Worker(join(__dirname, 'worker.js'), {
-  workerData: {
-    name: FIRST_PROCESS_NAME,
-    genIntervalBottom: FIRST_PROCESS_INTERVAL_BOTTON,
-    genIntervalTOP: FIRST_PROCESS_INTERVAL_TOP,
-    count: FIRST_PROCESS_COUNT,
-  },
-});
-
-const secondProcess = new Worker(join(__dirname, 'worker.js'), {
-  workerData: {
-    name: SECOND_PROCESS_NAME,
-    genIntervalBottom: SECOND_PROCESS_INTERVAL_BOTTON,
-    genIntervalTOP: SECOND_PROCESS_INTERVAL_TOP,
-    count: SECOND_PROCESS_COUNT,
-  },
-});
+const Model = require(join(__dirname, 'model.js'))
 
 const queue = new CPUQueue();
 const cpu = new CPU(90, 120, queue);
 
-let firstProcessDestroyed = 0;
-let secondProcessInterrupted = 0;
-let maxQueueLength = 0;
+const model = new Model(queue, cpu);
 
-firstProcess.on('message', () => {
-  console.log(`generated ${FIRST_PROCESS_NAME}`);
-  if (cpu.currProcess === SECOND_PROCESS_NAME) {
-    queue.add(SECOND_PROCESS_NAME);
-    if (maxQueueLength < queue.length) maxQueueLength = queue.length;
-    console.log(`INTERRUPTED ${SECOND_PROCESS_NAME}`);
-    clearTimeout(cpu.timeout);
-    secondProcessInterrupted++;
-  } else if (cpu.currProcess === FIRST_PROCESS_NAME) {
-    console.log(`DESTROYED ${FIRST_PROCESS_NAME}`);
-    clearTimeout(cpu.timeout);
-    firstProcessDestroyed++;
-  }
-  cpu.process(FIRST_PROCESS_NAME);
-});
-
-secondProcess.on('message', () => {
-  console.log(`generated ${SECOND_PROCESS_NAME}`);
-  queue.add(SECOND_PROCESS_NAME);
-  if (maxQueueLength < queue.length) maxQueueLength = queue.length;
-});
-
-Promise.all(
-  [firstProcess, secondProcess].map(
-    process => new Promise(res => process.on('exit', res))
-  )
-)
-  .then(() => new Promise(res => cpu.on('empty', res)))
-  .then(() => {
-    console.log(
-      `Destroyed first process: ${firstProcessDestroyed}/${FIRST_PROCESS_COUNT}`
-    );
-    console.log(`Interrupted second process: ${secondProcessInterrupted}`);
-    console.log(`Max length of queue: ${maxQueueLength}`);
-  });
+model.createProcesses();
+model.addProcessListeners();
+model.addResultsLog();
---
+++ 2/model.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const { Worker } = require('worker_threads');
+const join = require('path').join;
+
+const FIRST_PROCESS_NAME = 1;
+const FIRST_PROCESS_INTERVAL_BOTTON = 100;
+const FIRST_PROCESS_INTERVAL_TOP = 200;
+const FIRST_PROCESS_COUNT = 30;
+
+const SECOND_PROCESS_NAME = 2;
+const SECOND_PROCESS_INTERVAL_BOTTON = 100;
+const SECOND_PROCESS_INTERVAL_TOP = 200;
+const SECOND_PROCESS_COUNT = 30;
+
+class Model {
+  constructor(queue, cpu) {
+    this.queue = queue;
+    this.cpu = cpu;
+    this.firstProcessDestroyed = 0;
+    this.secondProcessInterrupted = 0;
+    this.maxQueueLength = 0;
+    this.firstProcess = null;
+    this.secondProcess = null;
+  }
+  createProcesses() {
+    this.firstProcess = new Worker(join(__dirname, 'worker.js'), {
+      workerData: {
+        name: FIRST_PROCESS_NAME,
+        genIntervalBottom: FIRST_PROCESS_INTERVAL_BOTTON,
+        genIntervalTOP: FIRST_PROCESS_INTERVAL_TOP,
+        count: FIRST_PROCESS_COUNT,
+      },
+    });
+
+    this.secondProcess = new Worker(join(__dirname, 'worker.js'), {
+      workerData: {
+        name: SECOND_PROCESS_NAME,
+        genIntervalBottom: SECOND_PROCESS_INTERVAL_BOTTON,
+        genIntervalTOP: SECOND_PROCESS_INTERVAL_TOP,
+        count: SECOND_PROCESS_COUNT,
+      },
+    });
+  }
+
+  addProcessListeners() {
+    this.firstProcess.on('message', () => {
+      console.log(`generated ${FIRST_PROCESS_NAME}`);
+      if (this.cpu.currProcess === SECOND_PROCESS_NAME) {
+        this.queue.add(SECOND_PROCESS_NAME);
+        if (this.maxQueueLength < this.queue.length)
+          this.maxQueueLength = this.queue.length;
+        console.log(`INTERRUPTED ${SECOND_PROCESS_NAME}`);
+        clearTimeout(this.cpu.timeout);
+        this.secondProcessInterrupted++;
+      } else if (this.cpu.currProcess === FIRST_PROCESS_NAME) {
+        console.log(`DESTROYED ${FIRST_PROCESS_NAME}`);
+        clearTimeout(this.cpu.timeout);
+        this.firstProcessDestroyed++;
+      }
+      this.cpu.process(FIRST_PROCESS_NAME);
+    });
+
+    this.secondProcess.on('message', () => {
+      console.log(`generated ${SECOND_PROCESS_NAME}`);
+      this.queue.add(SECOND_PROCESS_NAME);
+      if (this.maxQueueLength < this.queue.length)
+        this.maxQueueLength = this.queue.length;
+    });
+  }
+
+  addResultsLog() {
+    Promise.all(
+      [this.firstProcess, this.secondProcess].map(
+        process => new Promise(res => process.on('exit', res))
+      )
+    )
+      .then(() => new Promise(res => this.cpu.on('empty', res)))
+      .then(() => {
+        console.log(
+          `Destroyed first process: ${this.firstProcessDestroyed
+          }/${FIRST_PROCESS_COUNT}`
+        );
+        console.log(
+          `Interrupted second process: ${this.secondProcessInterrupted}`
+        );
+        console.log(`Max length of queue: ${this.maxQueueLength}`);
+      });
+  }
+}
+
+module.exports = Model;
