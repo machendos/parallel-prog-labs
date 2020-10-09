7b3f63e587d732e492d170932f85ba21b3f5a985
Nikita Machekhin <nicitamac@gmail.com>
2020-10-09 03:39:29 +0300

    fixup! Add 5 lab

--- 5/async/Main.java
+++ 5/async/Main.java
@@ -1,37 +1,63 @@
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
 
 public class Main {
 
   public static void main(String[] args) throws InterruptedException, ExecutionException {
-
-    final int ARRAYS_LENGTH = (int) Math.pow(10, 1);
+    
+    final int ARRAYS_LENGTH = (int) Math.pow(10, 5);
     final int ELEMENT_MIN = 1;
-    final int ELEMENT_MAX = 1000;
+    final int ELEMENT_MAX = 10000;
 
-    CompletableFuture<int[]> term = CompletableFuture.supplyAsync(() -> {
+    CompletableFuture<int[]> firstArrayProcess = CompletableFuture.supplyAsync(() -> {
 
       int[] array = new int[ARRAYS_LENGTH];
       for (int index = 0; index < array.length; index++) {
         array[index] = Math.round((float) Math.random() * ELEMENT_MAX) - ELEMENT_MIN;
       }
-      for (int element : array) {
-        System.out.println(element);
-      }
 
-      System.out.println("--");
-      int max = Arrays.stream(array).max().orElse(0);
-      System.out.println(max);
-      System.out.println("--");
+      int halfMax = Arrays.stream(array).max().orElse(0) / 2;
 
-      return new int[4];
+      int[] filtered = Arrays
+        .stream(array)
+        .filter(element -> element > halfMax)
+        .sorted()
+        .toArray();
+
+      return filtered;
     });
 
-    term.get();
+    CompletableFuture<int[]> secondArrayProcess = CompletableFuture.supplyAsync(() -> {
+      int[] array = new int[ARRAYS_LENGTH];
+      for (int index = 0; index < array.length; index++) {
 
+        array[index] = Math.round((float) Math.random() * ELEMENT_MAX) - ELEMENT_MIN;
+      }
+      int[] filtered = Arrays
+        .stream(array)
+        .filter(element -> element % 2 == 0)
+        .sorted()
+        .toArray();
+
+      return filtered;
+    });
+
+    CompletableFuture<int[]> combinedFuture = firstArrayProcess.thenCombine(secondArrayProcess,
+        (firstArrayFiltered, secondArrayFiltered) -> {
+
+          int[] finalFiltered = Arrays
+            .stream(secondArrayFiltered)
+            .filter(element ->
+              !IntStream.of(firstArrayFiltered).anyMatch(x -> x == element)
+            )
+            .toArray();
+          return finalFiltered;
+        }
+      );
+
+    combinedFuture.get();
   }
 
 }
