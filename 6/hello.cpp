a5b0de03e04c1bb8a87524eefa945255d627b6fd
Nikita Machekhin <nicitamac@gmail.com>
2020-10-09 06:02:45 +0300

    Add solve of 6 lab

---
+++ 6/hello.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <omp.h>
+#include <atomic>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+  int VECTOR_SIZE = 100;
+  int MAX = 100;
+  int MIN = 0;
+
+  int vector1[VECTOR_SIZE];
+  int vector2[VECTOR_SIZE];
+
+  for (int index = 0; index < VECTOR_SIZE; index++)
+  {
+    vector1[index] = rand() % (MAX - MIN) + MIN;
+    vector2[index] = rand() % (MAX - MIN) + MIN;
+  }
+
+  long sequenced = 0;
+
+  for (int i = 0; i < VECTOR_SIZE; i++)
+  {
+    sequenced += vector1[i] * vector2[i];
+  }
+
+  printf("SEQUENCED: %d  \n", sequenced);
+
+  long unsafed = 0;
+
+  std::atomic<int> sharedValue(0);
+#pragma omp parallel for
+  for (int i = 0; i < VECTOR_SIZE; i++)
+  {
+    unsafed += vector1[i] * vector2[i];
+    sharedValue.fetch_add(vector1[i] * vector2[i]);
+  }
+
+  printf("UNSAFED: %d  \n", unsafed);
+  printf("SAFED: %d  \n", sharedValue.load());
+
+  return 0;
+}
